ae5be629e10288f7d6c875b617f498fe93513c9a
nico24687 <theonlynico@hotmail.com>
2017-11-01 09:38:20 -0600

    reunion has location

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -4,4 +4,9 @@ require 'minitest/autorun'
 require './lib/reunion'
 
 class ReunionTest < Minitest::Test
+  def test_reunion_has_location 
+    reunion = Reunion.new("London")
+
+    assert_equal "London", reunion.location
+  end
 end
