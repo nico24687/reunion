aed10420337ead845857b89467276579865f3d38
nico24687 <theonlynico@hotmail.com>
2017-11-01 09:10:26 -0600

    activity has name

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -2,6 +2,16 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/activity'
+require 'pry'
 
 class ActivityTest < Minitest::Test
+
+  def test_activity_has_attributes
+    activity = Activity.new("hiking")
+
+    assert_equal "hiking", activity.name
+  end 
+
+  def test_activity_has_no_participants_by_default
+  end
 end
